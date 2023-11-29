edb90a569350eef708d0b4205039d9ff4831fb62
sthio90 <sthio90@gmail.com>
2023-11-29 04:28:09 +0000

    Wed, Nov 29, 2023, 2:28 PM +10:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -1,7 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    //TODO prompt user for for height between 1 to 8
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height >0 && height <9);
+
+    //TODO printf hashes at prompted height
+
 }
