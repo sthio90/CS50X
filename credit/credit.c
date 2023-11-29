883f700a750363200d9dbd45af7e7d8998124fad
sthio90 <sthio90@gmail.com>
2023-11-29 05:57:37 +0000

    Wed, Nov 29, 2023, 3:57 PM +10:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,7 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
+    //TODO prompt for input
+    long card = get_long("Number: ");
     
+
+    //TODO calculate checksum
+
+    //TODO check for card length and starting digits
 }
