2deba6f7b3beb89a5b105bffe7da550e51c7e648
sthio90 <sthio90@gmail.com>
2023-11-29 06:24:29 +0000

    Wed, Nov 29, 2023, 4:24 PM +10:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    //TODO prompt for input
+    // TODO prompt for input
     long card;
     do
     {
@@ -11,45 +11,59 @@ int main(void)
     }
     while (card < 0);
 
-    //TODO calculate checksum using Luhn's algo
+    // TODO calculate checksum using Luhn's algo
 
     int sum = 0;
     long cardNumber = card;
     int length = 0;
-    while (cardNumber > 0) {
+    while (cardNumber > 0)
+    {
         int digit = cardNumber % 10;
         length++;
-        if (length % 2 == 0) {
+        if (length % 2 == 0)
+        {
             digit *= 2;
             sum += digit / 10 + digit % 10;
-        } else {
+        }
+        else
+        {
             sum += digit;
         }
         cardNumber /= 10;
     }
 
-    //TODO check for card length and starting digits
+    // TODO check for card length and starting digits
 
-    if (sum % 10 == 0) {
+    if (sum % 10 == 0)
+    {
         // Getting the first two digits
-        while (card >= 100) {
+        while (card >= 100)
+        {
             card /= 10;
         }
         int firstTwoDigits = card;
 
-        if ((length == 15) && (firstTwoDigits == 34 || firstTwoDigits == 37)) {
+        if ((length == 15) && (firstTwoDigits == 34 || firstTwoDigits == 37))
+        {
             printf("AMEX\n");
-        } else if ((length == 16) && (firstTwoDigits >= 51 && firstTwoDigits <= 55)) {
+        }
+        else if ((length == 16) && (firstTwoDigits >= 51 && firstTwoDigits <= 55))
+        {
             printf("MASTERCARD\n");
-        } else if ((length == 13 || length == 16) && (firstTwoDigits / 10 == 4)) {
+        }
+        else if ((length == 13 || length == 16) && (firstTwoDigits / 10 == 4))
+        {
             printf("VISA\n");
-        } else {
+        }
+        else
+        {
             printf("INVALID\n");
         }
-    } else {
+    }
+    else
+    {
         printf("INVALID\n");
     }
 
     return 0;
-
 }
