b7c9e623f4daa74ca167a57278c36acfa1f0aa41
sthio90 <sthio90@gmail.com>
2023-11-29 04:12:34 +0000

    Wed, Nov 29, 2023, 2:12 PM +10:00

--- hello/hello.c
+++ hello/hello.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int main(void)
 {
-    //TODO ask user for name
+    // TODO ask user for name
     string name = get_string("What is your name? ");
     printf("hello, %s\n", name);
 }
