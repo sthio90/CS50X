956ce69ca954b78079cf95ac0838999c4ab680a6
sthio90 <sthio90@gmail.com>
2023-11-29 06:19:06 +0000

    Wed, Nov 29, 2023, 4:19 PM +10:00

--- credit/credit.c
+++ credit/credit.c
@@ -15,4 +15,45 @@ int main(void)
     //
 
     //TODO check for card length and starting digits
+
+
+    // Calculate checksum using Luhn's Algorithm
+    int sum = 0;
+    long cardNumber = card;
+    int length = 0;
+    while (cardNumber > 0) {
+        int digit = cardNumber % 10;
+        length++;
+        if (length % 2 == 0) {
+            digit *= 2;
+            sum += digit / 10 + digit % 10;
+        } else {
+            sum += digit;
+        }
+        cardNumber /= 10;
+    }
+
+    // Check for card length and starting digits
+    if (sum % 10 == 0) {
+        // Getting the first two digits
+        while (card >= 100) {
+            card /= 10;
+        }
+        int firstTwoDigits = card;
+
+        if ((length == 15) && (firstTwoDigits == 34 || firstTwoDigits == 37)) {
+            printf("AMEX\n");
+        } else if ((length == 16) && (firstTwoDigits >= 51 && firstTwoDigits <= 55)) {
+            printf("MASTERCARD\n");
+        } else if ((length == 13 || length == 16) && (firstTwoDigits / 10 == 4)) {
+            printf("VISA\n");
+        } else {
+            printf("INVALID\n");
+        }
+    } else {
+        printf("INVALID\n");
+    }
+
+    return 0;
+
 }
