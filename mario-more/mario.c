b6fde6099704adbea2471442f9c97581e4522527
sthio90 <sthio90@gmail.com>
2023-11-29 05:44:08 +0000

    Wed, Nov 29, 2023, 3:44 PM +10:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,37 +3,35 @@
 
 int main(void)
 {
-    //TODO prompt user for for height between 1 to 8
+    // TODO prompt user for for height between 1 to 8
     int height;
     do
     {
         height = get_int("Height: ");
     }
-    while (height <1 || height >8);
-    //TODO for loop
+    while (height < 1 || height > 8);
+    // TODO nested for loop
     for (int i = 0; i < height; i++)
     {
-        //TODO printf spaces (height - row - 1)
+        // TODO printf spaces (height - row - 1)
         for (int j = 0; j < height - i - 1; j++)
         {
             printf(" ");
         }
-        //TODO printf left hashes
+        // TODO printf left hashes
         for (int j = 0; j <= i; j++)
         {
             printf("#");
         }
-        //TODO printf gap
+        // TODO printf gap
 
-            printf("  ");
+        printf("  ");
 
-        //TODO printf right hashes
+        // TODO printf right hashes
         for (int j = 0; j <= i; j++)
         {
             printf("#");
         }
         printf("\n");
     }
-
 }
-
