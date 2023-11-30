f0ad9f91b7367c9503a9f4d1ba181bdc29e1770a
sthio90 <sthio90@gmail.com>
2023-11-30 08:54:10 +0000

    Thu, Nov 30, 2023, 6:54 PM +10:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -21,15 +21,15 @@ int main(void)
     // TODO: Print the winner
     if (score1 > score2)
     {
-        printf("Player 1 is the winner!\n");
+        printf("Player 1 wins!\n");
     }
     else if (score2 > score1)
     {
-        printf("Player 2 is the winner!\n");
+        printf("Player 2 wins!\n");
     }
     else
     {
-        printf("It is a tie!\n");
+        printf("Tie!\n");
     }
 }
 
