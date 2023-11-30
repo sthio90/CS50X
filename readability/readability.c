1e4323db72ec089213238edf6224e04c89cf290a
sthio90 <sthio90@gmail.com>
2023-11-30 23:37:30 +0000

    Fri, Dec 1, 2023, 9:37 AM +10:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,9 +1,21 @@
 #include <cs50.h>
+#include <ctype.h>
 #include <stdio.h>
+#include
+
+int count_letters(string text);
 
 int main(void)
 {
     //TODO take user text input and print
     string text = get_string("Text: ");
     printf("Text: %s\n", text);
+
+    //TODO run count_letters function and print
+    int count = count_letters(text);
+}
+
+int count_letters(string text)
+{
+    strlen(text);
 }
