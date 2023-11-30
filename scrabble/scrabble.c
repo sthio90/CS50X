de48a8c52bcaf368f601094ca2f23230e3085e3c
sthio90 <sthio90@gmail.com>
2023-11-30 08:52:06 +0000

    Thu, Nov 30, 2023, 6:52 PM +10:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -41,8 +41,7 @@ int compute_score(string word)
     // iterate over each character in the string
     for (int i = 0; i < wordLength; i++)
     {
-
-    // assign points to chars in string and 0 to non-letters and sum
+         // convert cahr to uppercase if it's a letter and adds sum
         if (isalpha(word[i]))
         {
             word[i] = toupper(word[i]);
