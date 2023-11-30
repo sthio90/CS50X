7113bc8cb1b85eb0f37d3670ff979724d836bc3f
sthio90 <sthio90@gmail.com>
2023-11-30 06:13:17 +0000

    Thu, Nov 30, 2023, 4:13 PM +10:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -31,7 +31,7 @@ int compute_score(string word)
     {
         if (char of the word is lower case)
         {
-            change to uppercase;
+            char uppercase;
         }
 
     // assign points to chars in string and 0 to non-letters and sum
