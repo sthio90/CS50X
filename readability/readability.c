4ad09b906d309b763d28094e38939b09f3369d44
sthio90 <sthio90@gmail.com>
2023-12-01 00:00:38 +0000

    Fri, Dec 1, 2023, 10:00 AM +10:00

--- readability/readability.c
+++ readability/readability.c
@@ -15,6 +15,10 @@ int main(void)
     //TODO run count_letters function and print
     int count = count_letters(text);
     printf("%i letters\n", count);
+
+    //TODO count words and print
+    int words = count_words(text);
+    printf("%i words\n", words);
 }
 
 int count_letters(string text)
@@ -33,9 +37,15 @@ int count_letters(string text)
 
 int count_words(string text)
 {
+    // We can assume there will be 1 word, single spaces and the string does not start or end in a space
+    int sumWords = 1;
     //TODO for loop to count words
     for (int i = 0; i < strlen(text); i++)
     {
-        
+        if (isspace(text[i]))
+        {
+            sumWords++;
+        }
     }
+    return sumWords;
 }
