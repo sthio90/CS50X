4ba0a0c3156627a9f3712ac40520cf217ac63849
sthio90 <sthio90@gmail.com>
2023-12-01 02:32:18 +0000

    Fri, Dec 1, 2023, 12:32 PM +10:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -12,32 +12,33 @@ int main(void)
     string message = get_string("Message: ");
 
     // TODO convert message into series of 8bit binary numbers one for each character
-    for (int i = 0; i < strlen(message); i++) {
+    for (int i = 0; i < strlen(message); i++)
+    {
         char c = message[i];
         int mask = 128; // Binary 10000000
 
-        for (int j = 0; j < BITS_IN_BYTE; j++) {
+        for (int j = 0; j < BITS_IN_BYTE; j++)
+        {
             int bit = (c & mask) ? 1 : 0;
             print_bulb(bit); // Prints emojis
-            // printf("%d", bit);
             mask >>= 1;
         }
 
-    printf("\n");
+        printf("\n");
     }
 
-
     // TODO print series of emojis with print_bulb function
-
 }
 
 void print_bulb(int bit)
 {
-    if (bit == 0) {
+    if (bit == 0)
+    {
         // Dark emoji
         printf("\U000026AB");
     }
-    else if (bit == 1) {
+    else if (bit == 1)
+    {
         // Light emoji
         printf("\U0001F7E1");
     }
