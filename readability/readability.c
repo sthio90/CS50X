f73cbc362ab7c16d07b2ea3a37043b0bc87e0119
sthio90 <sthio90@gmail.com>
2023-12-01 00:25:19 +0000

    Fri, Dec 1, 2023, 10:25 AM +10:00

--- readability/readability.c
+++ readability/readability.c
@@ -11,29 +11,25 @@ int clInd(int count, int words, int sent);
 
 int main(void)
 {
-    //TODO take user text input and print
+    // TODO take user text input and print
     string text = get_string("Text: ");
-    printf("Text: %s\n", text);
 
-    //TODO run count_letters function and print
+    // TODO run count_letters function and print
     int count = count_letters(text);
-    printf("%i letters\n", count);
 
-    //TODO count words and print
+    // TODO count words and print
     int words = count_words(text);
-    printf("%i words\n", words);
 
-    //TODO count sentences and print
+    // TODO count sentences and print
     int sent = count_sentences(text);
-    printf("%i sentences\n", sent);
 
-    //TODO use clInd to get grading and print
+    // TODO use clInd to get grading and print
     int grade = clInd(count, words, sent);
     if (grade > 16)
     {
         printf("Grade 16+\n");
     }
-    else if (grade <1)
+    else if (grade < 1)
     {
         printf("Before Grade 1\n");
     }
@@ -61,7 +57,7 @@ int count_words(string text)
 {
     // We can assume there will be 1 word, single spaces and the string does not start or end in a space
     int sumWords = 1;
-    //TODO for loop to count words
+    // TODO for loop to count words
     for (int i = 0; i < strlen(text); i++)
     {
         if (isspace(text[i]))
@@ -76,10 +72,10 @@ int count_sentences(string text)
 {
     int sumSent = 0;
 
-    //TODO for loop to count sentences separated by . ! ?
+    // TODO for loop to count sentences separated by . ! ?
     for (int i = 0; i < strlen(text); i++)
     {
-        if(ispunct(text[i]))
+        if (ispunct(text[i]))
         {
             sumSent++;
         }
@@ -90,7 +86,6 @@ int count_sentences(string text)
 int clInd(int count, int words, int sent)
 {
     // Coleman-Liau index = 0.0588 * L - 0.296 * S - 15.8; L is ave letters/100 words and S is ave sentences /100 words
-    float clGrade = 0.0588 * (count * 100 /words) - 0.296 * (sent * 100 / words);
+    float clGrade = 0.0588 * (count * 100 / words) - 0.296 * (sent * 100 / words);
     return round(clGrade);
-
 }
