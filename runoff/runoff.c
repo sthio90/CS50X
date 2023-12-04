192e385368bb8042be799be47f54b21a2766beb6
sthio90 <sthio90@gmail.com>
2023-12-04 02:21:42 +0000

    Mon, Dec 4, 2023, 12:21 PM +10:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -127,7 +127,10 @@ int main(int argc, string argv[])
 bool vote(int voter, int rank, string name)
 {
     // TODO
-    
+    for (i = 0; i < candidate_count; i++)
+    {
+        
+    }
     return false;
 }
 
