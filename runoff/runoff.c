1bcecd6505666bf4259225526f5b4271b24e9729
sthio90 <sthio90@gmail.com>
2023-12-04 03:32:44 +0000

    Mon, Dec 4, 2023, 1:32 PM +10:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -176,8 +176,8 @@ bool print_winner(void)
         if (candidates[i].votes > (voter_count / 2) )
         {
             printf ("%s\n", candidates[i].name);
-            return true;
         }
+        return true;
     }
         return false;
 }
