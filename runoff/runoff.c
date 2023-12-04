6c7625c394af8a4ca4dd567ab3cfc5e3e87ddae9
sthio90 <sthio90@gmail.com>
2023-12-04 03:27:48 +0000

    Mon, Dec 4, 2023, 1:27 PM +10:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -229,5 +229,12 @@ bool is_tie(int min)
 void eliminate(int min)
 {
     // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].votes == min)
+        {
+            candidates[i].eliminated = true;
+        }
+    }
     return;
 }
