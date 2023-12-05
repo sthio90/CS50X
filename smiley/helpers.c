c1d96d491e45c9cd8e53395c6f0a5da364e8c71f
sthio90 <sthio90@gmail.com>
2023-12-05 04:26:06 +0000

    Tue, Dec 5, 2023, 2:26 PM +10:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -4,12 +4,12 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
     // for loop to go through each pixel
-    for (int i = 0; i < height * width; i++)
+    for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
         {
             // if pixel is black
-            if(image[i][j].rgbtBlue == "00" && image[i][j].rgbtGreen == "00" && image[i][j].rgbtRed == "00")
+            if(image[i][j].rgbtBlue == 00" && image[i][j].rgbtGreen == "00" && image[i][j].rgbtRed == "00")
             {
                 // change pixel to different colour
                 image[i][j].rgbtBlue = "ED";
