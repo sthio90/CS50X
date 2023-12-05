ab41fb27fac85c8a9099af71eff860e9d13072b6
sthio90 <sthio90@gmail.com>
2023-12-05 06:25:46 +0000

    Tue, Dec 5, 2023, 4:25 PM +10:00

--- volume/volume.c
+++ volume/volume.c
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
 
     // TODO: Read samples from input file and write updated data to output file
     int16_t buffer;
-    while (fread(&buffer, sizeof(int16_t), 1, input)) //while loop over sample
+    while (fread(&buffer, sizeof(int16_t), 1, input)) // while loop over sample
     {
         buffer *= factor;
         fwrite(&buffer, sizeof(int16_t), 1, output);
