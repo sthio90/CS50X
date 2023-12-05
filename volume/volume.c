0c87b88b63c0ea91f6f14c6ae881ef4bda84a484
sthio90 <sthio90@gmail.com>
2023-12-05 06:05:07 +0000

    Tue, Dec 5, 2023, 4:05 PM +10:00

--- volume/volume.c
+++ volume/volume.c
@@ -36,9 +36,10 @@ int main(int argc, char *argv[])
     // TODO: Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
     // read header from input file
-    fread(header, sizeof(char), 1, input)
+    fread(header, sizeof(char), HEADER_SIZE, input);
     // write header in output file
-    fwrite()
+    fwrite(header, sizeof(char), HEADER_SIZE, output);
+
     // TODO: Read samples from input file and write updated data to output file
 
     // Close files
