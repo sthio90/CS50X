02ab4872244d5e7de8908824581d0eed1b2e0591
sthio90 <sthio90@gmail.com>
2023-12-06 06:24:50 +0000

    Wed, Dec 6, 2023, 4:24 PM +10:00

--- recover/recover.c
+++ recover/recover.c
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
             // if first jpeg
-            if (!img == NULL)
+            if (img != NULL)
             {
                 // close file
                 fclose(img);
