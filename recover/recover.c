1d733867ab3e9eb698c406aa9bc52e24a81650ae
sthio90 <sthio90@gmail.com>
2023-12-06 05:36:14 +0000

    Wed, Dec 6, 2023, 3:36 PM +10:00

--- recover/recover.c
+++ recover/recover.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
     FILE *file = fopen(argv[1], "r");
     if (file == NULL)
     {
-        fprint("Unable to open file.\n");
+        printf("Unable to open file.\n");
         return 2;
     }
 
