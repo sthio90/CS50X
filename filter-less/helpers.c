cd92bcdba86eeace7ae08394af46dc93e2476058
sthio90 <sthio90@gmail.com>
2023-12-06 02:57:46 +0000

    Wed, Dec 6, 2023, 12:57 PM +10:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -1,9 +1,11 @@
 #include "helpers.h"
 
-double average(int arr[], int size) {
+double average(int arr[], int size)
+{
     double sum = 0.0;
 
-    for (int i = 0; i < size; i++) {
+    for (int i = 0; i < size; i++)
+    {
         sum += arr[i];
     }
 
@@ -25,19 +27,14 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
         for (int j = 0; j < width; j++)
         {
             // Extract RGB values
-            int rgbValues[3] =
-            {
-                image[i][j].rgbtBlue,
-                image[i][j].rgbtGreen,
-                image[i][j].rgbtRed
-            };
+            int rgbValues[3] = {image[i][j].rgbtBlue, image[i][j].rgbtGreen, image[i][j].rgbtRed};
 
-        // average BGR values of pixels and find average
-        int ave = (int)(average(rgbValues, 3) + 0.5);
-        // set values to average
-        image[i][j].rgbtBlue = ave;
-        image[i][j].rgbtGreen = ave;
-        image[i][j].rgbtRed = ave;
+            // average BGR values of pixels and find average
+            int ave = (int) (average(rgbValues, 3) + 0.5);
+            // set values to average
+            image[i][j].rgbtBlue = ave;
+            image[i][j].rgbtGreen = ave;
+            image[i][j].rgbtRed = ave;
         }
     }
     return;
@@ -56,18 +53,18 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             int originalBlue = image[i][j].rgbtBlue;
 
             // Calculate sepia tones
-            int sepiaRed = (int)(.393 * originalRed + .769 * originalGreen + .189 * originalBlue + 0.5);
-            int sepiaGreen = (int)(.349 * originalRed + .686 * originalGreen + .168 * originalBlue + 0.5);
-            int sepiaBlue = (int)(.272 * originalRed + .534 * originalGreen + .131 * originalBlue + 0.5);
+            int sepiaRed = (int) (.393 * originalRed + .769 * originalGreen + .189 * originalBlue + 0.5);
+            int sepiaGreen = (int) (.349 * originalRed + .686 * originalGreen + .168 * originalBlue + 0.5);
+            int sepiaBlue = (int) (.272 * originalRed + .534 * originalGreen + .131 * originalBlue + 0.5);
 
             // Clamp values to max 255
             image[i][j].rgbtRed = sepiaRed > 255 ? 255 : sepiaRed;
             image[i][j].rgbtGreen = sepiaGreen > 255 ? 255 : sepiaGreen;
             image[i][j].rgbtBlue = sepiaBlue > 255 ? 255 : sepiaBlue;
         }
-    // sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue
-    // sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue
-    // sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue
+        // sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue
+        // sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue
+        // sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue
     }
     return;
 }
@@ -108,15 +105,15 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
             int totalRed = 0, totalGreen = 0, totalBlue = 0;
             int count = 0;
 
-            //find the pixels 1 column and 1 row away and average all of them
+            // find the pixels 1 column and 1 row away and average all of them
             for (int di = -1; di <= 1; di++)
             {
-                for (int dj = -1; dj <=1; dj++)
+                for (int dj = -1; dj <= 1; dj++)
                 {
                     int newI = i + di;
                     int newJ = j + dj;
 
-                    if (newI >=0 && newI < height && newJ >= 0 && newJ < width)
+                    if (newI >= 0 && newI < height && newJ >= 0 && newJ < width)
                     {
                         totalRed += copy[newI][newJ].rgbtRed;
                         totalGreen += copy[newI][newJ].rgbtGreen;
@@ -126,13 +123,11 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                 }
             }
 
-
-        // Average of each component
-        image[i][j].rgbtRed = (totalRed + count / 2) / count;
-        image[i][j].rgbtGreen = (totalGreen + count / 2) / count;
-        image[i][j].rgbtBlue = (totalBlue + count / 2) / count;
+            // Average of each component
+            image[i][j].rgbtRed = (totalRed + count / 2) / count;
+            image[i][j].rgbtGreen = (totalGreen + count / 2) / count;
+            image[i][j].rgbtBlue = (totalBlue + count / 2) / count;
         }
     }
     return;
 }
-
