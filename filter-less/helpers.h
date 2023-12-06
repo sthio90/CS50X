47adcd22a3c868965c13ea0728c1f6e6180beeb0
sthio90 <sthio90@gmail.com>
2023-12-06 00:47:26 +0000

    Wed, Dec 6, 2023, 10:47 AM +10:00

--- filter-less/helpers.h
+++ filter-less/helpers.h
@@ -1,13 +1,14 @@
 #include "bmp.h"
 
-// Convert image to grayscale
+// Convert image to grayscale - average BGR and set values to ave
 void grayscale(int height, int width, RGBTRIPLE image[height][width]);
+    //
 
-// Convert image to sepia
+// Convert image to sepia - convert BGR using formula
 void sepia(int height, int width, RGBTRIPLE image[height][width]);
 
-// Reflect image horizontally
+// Reflect image horizontally - swap pixels left to right
 void reflect(int height, int width, RGBTRIPLE image[height][width]);
 
-// Blur image
+// Blur image - average pixels with 1 column and 1 row pixels away
 void blur(int height, int width, RGBTRIPLE image[height][width]);
