39f3906478c139b7442017fd8f714a81e7e36591
sthio90 <sthio90@gmail.com>
2023-12-06 01:13:29 +0000

    Wed, Dec 6, 2023, 11:13 AM +10:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -39,18 +39,38 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia - convert BGR using formula
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            
+        }
+    // sepiaRed = .393 * originalRed + .769 * originalGreen + .189 * originalBlue
+    // sepiaGreen = .349 * originalRed + .686 * originalGreen + .168 * originalBlue
+    // sepiaBlue = .272 * originalRed + .534 * originalGreen + .131 * originalBlue
+    }
     return;
 }
 
 // Reflect image horizontally - swap pixels left to right
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    // for (int i = 0; i < height; i++)
+    // {
+    //     for (int j = 0; j < width; j++)
+    //     {}
+    // }
     return;
 }
 
 // Blur image - average pixels with 1 column and 1 row pixels away
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    // for (int i = 0; i < height; i++)
+    // {
+    //     for (int j = 0; j < width; j++)
+    //     {}
+    // }
     return;
 }
 
