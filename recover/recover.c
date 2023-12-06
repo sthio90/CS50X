47aca5657e513bad51b1c70ca88ed6c4b700a055
sthio90 <sthio90@gmail.com>
2023-12-06 05:27:12 +0000

    Wed, Dec 6, 2023, 3:27 PM +10:00

--- recover/recover.c
+++ recover/recover.c
@@ -8,13 +8,18 @@ const BYTE BLOCK_SIZE = 512;
 
 int main(int argc, char *argv[])
 {
+    if (argc != 2)
+    {
+        printf("Improper usage.");
+        return 1;
+    }
 
     // open files
     FILE *file = fopen(argv[1], "r");
     if (file == NULL)
     {
         fprint("Unable to open file.");
-        return 1;
+        return 2;
     }
 
     while (fread(buffer, 1, BLOCK_SIZE, argv[1]) == BLOCK_SIZE) //repeat until end of card:
