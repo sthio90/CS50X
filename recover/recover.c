943da49012aae2050aeec4b10d491929b62ffb87
sthio90 <sthio90@gmail.com>
2023-12-06 04:40:55 +0000

    Wed, Dec 6, 2023, 2:40 PM +10:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
 int main(int argc, char *argv[])
 {
- 
+ // Define bytes and types
+ // open files
+ // fread file
+    // if header of jpeg
+        // fwrite 
 }
