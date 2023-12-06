689607b6352f434fd204a55648cd3e084b3012e6
sthio90 <sthio90@gmail.com>
2023-12-06 04:56:51 +0000

    Wed, Dec 6, 2023, 2:56 PM +10:00

--- recover/recover.c
+++ recover/recover.c
@@ -16,12 +16,14 @@ int main(int argc, char *argv[])
     fprint("Unable to open file.");
     return 1;
  }
+    //repeat until end of card:
+        // fread file in 512 byte chunks
+        // if header of jpeg
+            // fwrite into new output in 512 byte chunks
+            // if fread returns < 512 then end of file and return
+        // else (already in jpeg)
+            // keep writing
 
- // fread file
-    // if header of jpeg
-        // fwrite into new output in 512 byte chunks
-        // if fread returns < 512 then end of file and return
-    // else (already in jpeg)
-        // keep writing
+    //close any remaining files
 
 }
