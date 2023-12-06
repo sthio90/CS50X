45d5dd5dc6226f01b859909d9f9f67e01d42f9fa
sthio90 <sthio90@gmail.com>
2023-12-06 04:43:16 +0000

    Wed, Dec 6, 2023, 2:43 PM +10:00

--- recover/recover.c
+++ recover/recover.c
@@ -4,10 +4,12 @@
 int main(int argc, char *argv[])
 {
  // Define bytes and types
+ typedef uint8_t BYTE;
  // open files
  // fread file
     // if header of jpeg
-        // fwrite into new outputin 512 byte chunks
-    // else
+        // fwrite into new output in 512 byte chunks
+    // else (already in jpeg)
+        // keep writing
 
 }
