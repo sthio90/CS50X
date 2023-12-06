17078e9100cfe9923fc45e80af13af76b2891dbd
sthio90 <sthio90@gmail.com>
2023-12-06 23:58:49 +0000

    Thu, Dec 7, 2023, 9:58 AM +10:00

--- recover/recover.c
+++ recover/recover.c
@@ -32,14 +32,13 @@ int main(int argc, char *argv[])
         // if header of new jpeg
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
         {
-            // if first jpeg
+            // if not first jpeg
             if (img != NULL)
             {
                 // close file
                 fclose(img);
 
             }
-            // if not first jpeg
             // write new jpeg file
             sprintf(filename, "%03i.jpg", fileCount++);
             img = fopen(filename, "w");
