ead135b7fd579eec0c56c0b949c77d9e959c6a1f
sthio90 <sthio90@gmail.com>
2023-12-11 09:49:21 +0000

    Mon, Dec 11, 2023, 7:49 PM +10:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -12,7 +12,20 @@ def main():
             print("Invalid input. Please enter a whole number.")
 
     # Print out blocks of height
-    
+    print_blocks(height)
+
+def print_blocks(height):
+    # Print spaces, L blocks, gap, R blocks
+    for i in range(height):
+        for j in range(height - i - 1): # Spaces
+            print(" ")
+        for j in range(height + 1) # L blocks
+            print("#")
+
+        print("  ") # Gap
+
+        for j in range(height + 1) # R blocks
+            print("#")
 
 
 if __name__ == "__main__":
