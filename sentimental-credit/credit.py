14e5580830f2f4e2aafc8af9f0b419d263fb5b4d
sthio90 <sthio90@gmail.com>
2023-12-11 10:09:28 +0000

    Mon, Dec 11, 2023, 8:09 PM +10:00

--- sentimental-credit/credit.py
+++ sentimental-credit/credit.py
@@ -16,6 +16,60 @@ def main():
     # checksum
     # print card type
 
+        // TODO calculate checksum using Luhn's algo
+    int sum = 0;
+    long cardNumber = card;
+    int length = 0;
+    while (cardNumber > 0)
+    {
+        int digit = cardNumber % 10;
+        length++;
+        if (length % 2 == 0)
+        {
+            digit *= 2;
+            sum += digit / 10 + digit % 10;
+        }
+        else
+        {
+            sum += digit;
+        }
+        cardNumber /= 10;
+    }
+
+    // TODO check for card length and starting digits
+    if (sum % 10 == 0)
+    {
+        // TODO Getting the first two digits
+        while (card >= 100)
+        {
+            card /= 10;
+        }
+        int firstTwoDigits = card;
+
+        if ((length == 15) && (firstTwoDigits == 34 || firstTwoDigits == 37))
+        {
+            printf("AMEX\n");
+        }
+        else if ((length == 16) && (firstTwoDigits >= 51 && firstTwoDigits <= 55))
+        {
+            printf("MASTERCARD\n");
+        }
+        else if ((length == 13 || length == 16) && (firstTwoDigits / 10 == 4))
+        {
+            printf("VISA\n");
+        }
+        else
+        {
+            printf("INVALID\n");
+        }
+    }
+    else
+    {
+        printf("INVALID\n");
+    }
+
+    return 0;
+
 def checksum(card):
     return
 
