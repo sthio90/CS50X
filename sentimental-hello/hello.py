be9888877fa65ecf3eb0836e71fcc45e4e3bfec3
sthio90 <sthio90@gmail.com>
2023-12-11 09:24:55 +0000

    Mon, Dec 11, 2023, 7:24 PM +10:00

--- sentimental-hello/hello.py
+++ sentimental-hello/hello.py
@@ -1,9 +1,16 @@
 # TODO
 def main():
-    name = input("What is your name? ")
-    name = name.strip().capitalize()
-    print(f"hello, {name}")
+    while True:
+        name = input("What is your name? ")
+        name = name.strip()
 
+        # Check if the name contains only letters and possibly spaces
+        if name.replace(" ", "").isalpha():
+            name = name.capitalize()
+            print(f"hello, {name}")
+            break
+        else:
+            print("Invalid input. Please enter a valid name.")
 
 if __name__ == "__main__":
     main()
