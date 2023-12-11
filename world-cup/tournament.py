4eb1b822327a678d71481ec4c11c104bf1f0cde1
sthio90 <sthio90@gmail.com>
2023-12-11 07:17:35 +0000

    Mon, Dec 11, 2023, 5:17 PM +10:00

--- world-cup/tournament.py
+++ world-cup/tournament.py
@@ -16,6 +16,8 @@ def main():
 
     teams = []
     # TODO: Read teams into memory from file
+    open(sys.argv[1])
+    
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
