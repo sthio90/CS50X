2bc975734475591fa255b84a0698b698c4e2cf78
sthio90 <sthio90@gmail.com>
2023-12-11 09:32:47 +0000

    Mon, Dec 11, 2023, 7:32 PM +10:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,1 +1,10 @@
 # TODO
+def main():
+    # Get user height input
+    height = int(input("Height: "))
+    if height < 1 or height > 8:
+        height = int(input("Height: "))
+
+
+if __name__ == "__main__":
+    main()
