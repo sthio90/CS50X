e2e999a7d54e6325a2ae2442276b2eb0b87e899c
sthio90 <sthio90@gmail.com>
2023-12-12 03:01:39 +0000

    Tue, Dec 12, 2023, 1:01 PM +10:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,5 +1,6 @@
 import re
 
+
 def main():
     # Take user text input
     text = input("Text: ")
@@ -20,20 +21,25 @@ def main():
     else:
         print(f"Grade {grade}")
 
+
 def count_letters(text):
     return sum(1 for char in text if char.isalpha())
+
 
 def count_words(text):
     return len(text.split())
 
+
 def count_sentences(text):
     return len(re.findall(r"[.!?]", text))
+
 
 def cl_index(letter_count, word_count, sentence_count):
     L = letter_count * 100.0 / word_count
     S = sentence_count * 100.0 / word_count
     cl_grade = 0.0588 * L - 0.296 * S - 15.8
     return round(cl_grade)
+
 
 if __name__ == "__main__":
     main()
