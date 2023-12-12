70148b26f48734447d20cce17934c269928c631c
sthio90 <sthio90@gmail.com>
2023-12-12 04:02:11 +0000

    Tue, Dec 12, 2023, 2:02 PM +10:00

--- dna/dna.py
+++ dna/dna.py
@@ -25,10 +25,13 @@ def main():
 
 
     # TODO: Find longest match of each STR in DNA sequence
-    longest_match(dna_sequence, database["])
+    # AGATC,TTTTTTCT,AATG,TCTAG,GATA,TATC,GAAA,TCTG
+    agatc = longest_match(dna_sequence, "AGATC")
+    TTTTTTCT = longest_match(dna_sequence, "TTTTTTCT")
+
 
     # TODO: Check database for matching profiles
-
+    
     return
 
 
