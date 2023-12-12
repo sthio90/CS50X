39dda6c5cb6581aeafe0c6c7a12679532603095b
sthio90 <sthio90@gmail.com>
2023-12-12 04:32:55 +0000

    Tue, Dec 12, 2023, 2:32 PM +10:00

--- dna/dna.py
+++ dna/dna.py
@@ -3,7 +3,6 @@ import sys
 
 
 def main():
-
     # TODO: Check for command-line usage
     if len(sys.argv) != 3:
         print("Usage: python dna.py data.csv sequence.txt")
@@ -47,7 +46,6 @@ def longest_match(sequence, subsequence):
 
     # Check each character in sequence for most consecutive runs of subsequence
     for i in range(sequence_length):
-
         # Initialize count of consecutive runs
         count = 0
 
@@ -55,7 +53,6 @@ def longest_match(sequence, subsequence):
         # If a match, move substring to next potential match in sequence
         # Continue moving substring and checking for matches until out of consecutive matches
         while True:
-
             # Adjust substring start and end
             start = i + count * subsequence_length
             end = start + subsequence_length
