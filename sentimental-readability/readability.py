381483e1ce872b4496c33a192efb1d9e9bc45774
sthio90 <sthio90@gmail.com>
2023-12-12 02:58:20 +0000

    Tue, Dec 12, 2023, 12:58 PM +10:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -1,1 +1,42 @@
-# TODO
+import re
+
+def main():
+    # Take user text input
+    text = input("Text: ")
+
+    # Count letters, words, and sentences
+    count = count_letters(text)
+    print(count)
+
+    words = count_words(text)
+    print(words)
+
+    sentences = count_sentences(text)
+    print(sentences)
+
+    # Calculate and print grade
+    grade = cl_index(count, words, sentences)
+    if grade > 16:
+        print("Grade 16+")
+    elif grade < 1:
+        print("Before Grade 1")
+    else:
+        print(f"Grade {grade}")
+
+def count_letters(text):
+    return sum(1 for char in text if char.isalpha())
+
+def count_words(text):
+    return len(text.split())
+
+def count_sentences(text):
+    return len(re.findall(r"[.!?]", text))
+
+def cl_index(letter_count, word_count, sentence_count):
+    L = letter_count * 100.0 / word_count
+    S = sentence_count * 100.0 / word_count
+    cl_grade = 0.0588 * L - 0.296 * S - 15.8
+    return round(cl_grade)
+
+if __name__ == "__main__":
+    main()
