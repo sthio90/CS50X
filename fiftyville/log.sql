13d8608c5f7d54bde163ba8c0c564a896c140928
sthio90 <sthio90@gmail.com>
2023-12-13 10:50:03 +0000

    Wed, Dec 13, 2023, 8:50 PM +10:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,5 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- Looking up crime scene reports for the 28th of July 2021 at Humphrey Street
+SELECT * FROM crime_scene_reports
+    WHERE year = 2021 AND month = 7 AND day = 28 AND street = 'Humphrey Street';
