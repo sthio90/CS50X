a17f0db8d8f8417966deb4d5f6479e6856b748fd
sthio90 <sthio90@gmail.com>
2023-12-13 08:37:51 +0000

    Wed, Dec 13, 2023, 6:37 PM +10:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,1 +1,5 @@
-SELECT DISTINCT name FROM people WHERE people.id IN (SELECT directors.person_id FROM directors JOIN ratings ON directors.movie_id = ratings.movie_id WHERE rating > 9.0);
+SELECT DISTINCT p.name
+FROM people p
+JOIN directors d ON p.id = d.person_id
+JOIN ratings r ON d.movie_id = r.movie_id
+WHERE r.rating >= 9.0;
