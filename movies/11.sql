d12aaa7c5702dab7aca756da9d13aab13a486502
sthio90 <sthio90@gmail.com>
2023-12-13 08:21:53 +0000

    Wed, Dec 13, 2023, 6:21 PM +10:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,1 +1,11 @@
-SELECT title FROM movies WHERE movies.id IN (SELECT stars.movie_id FROM stars INNER JOIN people ON stars.person_id = people.id INNER JOIN ratings ON ratings.movie_id =  stars.movie_id WHERE people.name = 'Chadwick Boseman' ORDER BY rating DESC) LIMIT 5;
+SELECT title
+FROM movies
+INNER JOIN ratings ON movies.id = ratings.movie_id
+WHERE movies.id IN (
+    SELECT stars.movie_id
+    FROM stars
+    INNER JOIN people ON stars.person_id = people.id
+    WHERE people.name = 'Chadwick Boseman'
+)
+ORDER BY ratings.rating DESC
+LIMIT 5;
