25f1cb069a2ff6510ae7f2078fe43f993865241c
sthio90 <sthio90@gmail.com>
2023-12-13 10:57:08 +0000

    Wed, Dec 13, 2023, 8:57 PM +10:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -7,5 +7,6 @@ SELECT * FROM crime_scene_reports
 -- | 295 | 2021 | 7     | 28  | Humphrey Street | Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery. |
 -- | 297 | 2021 | 7     | 28  | Humphrey Street | Littering took place at 16:36. No known witnesses.
 
--- Identify people who were near Humphrey Street on July 28, 2021
-SELECT * FROM bakery_security_logs WHERE date = '2021-07-28';
+-- Find all activities at the bakery on July 28, 2021
+SELECT * FROM bakery_security_logs
+    WHERE year = 2021 AND month = 7 AND day = 28;
