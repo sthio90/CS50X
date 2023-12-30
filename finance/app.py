6dcc9d8315aa9483e60dab8bc869ee26347815f7
sthio90 <sthio90@gmail.com>
2023-12-30 22:24:13 +0000

    Sun, Dec 31, 2023, 8:24 AM +10:00

--- finance/app.py
+++ finance/app.py
@@ -60,7 +60,8 @@ def buy():
         # Update user's cash
         db.execute("UPDATE users SET cash = cash - ? WHERE id = ?", total_cost, session["user_id"])
 
-        # Record the purchase in transactions table (assuming it exists)
+        # Record the purchase in transactions table
+        
         # db.execute("INSERT INTO transactions (user_id, symbol, shares, price, type) VALUES (?, ?, ?, ?, 'buy')", session["user_id"], symbol, shares, stock["price"])
 
         # Update the user's portfolio
