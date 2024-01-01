8ba1e5d7f9ff8d93db075cc51c7b7d9166f67a48
sthio90 <sthio90@gmail.com>
2024-01-01 00:42:44 +0000

    Mon, Jan 1, 2024, 10:42 AM +10:00

--- project/app.py
+++ project/app.py
@@ -19,7 +19,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure CS50 Library to use SQLite database
-db = SQL("sqlite:///finance.db")
+db = SQL("sqlite:///project.db")
 
 
 @app.after_request
@@ -365,7 +365,7 @@ def crypto():
     # Handle GET request
     return render_template("crypto.html", symbol=None, crypto_price=None)
 
-    
+
 @app.route("/add_crypto", methods=["GET", "POST"])
 @login_required
 def add_crypto():
